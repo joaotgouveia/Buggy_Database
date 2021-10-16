1f6960d1ba2b96e2ec7e5dc9a3a33a7cad1bab18
João Gouveia <jjtucki4@gmail.com>
2021-10-16 15:56:31 +0100

    Finished corrigir_palavras function
    Needs testing

--- Project.py
+++ Project.py
@@ -1,19 +1,27 @@
 def corrigir_palavra(sCadCarateres):
-    dCadCarateres = {}
-    lKeys = []
+    lCadCarateres = []
     bChanged = True
+    sCorrected = ""
     for i in range(len(sCadCarateres)):
-        dCadCarateres[i] = sCadCarateres[i].lower()
-    print(dCadCarateres)
-    input()
+        lCadCarateres += sCadCarateres[i]
     while bChanged:
         bChanged = False
-        for i in range(len(dCadCarateres ) - 1):
-            if dCadCarateres[i] == dCadCarateres[i + 1]:
-                dCadCarateres.pop(i)
-                dCadCarateres.pop(i+1)
-                i += 1
-                bChanged = True
-    return dCadCarateres
-while True:
-    print(corrigir_palavra(input()))
+        for i in range(len(sCadCarateres) - 1):
+            if lCadCarateres[i] != 0 and lCadCarateres[i+1] != 0:
+                if lCadCarateres[i].lower() == lCadCarateres[i+1].lower() and ((lCadCarateres[i].islower() and lCadCarateres[i+1].isupper()) or (lCadCarateres[i+1].islower() and lCadCarateres[i].isupper())):
+                    lCadCarateres[i] = 0
+                    lCadCarateres[i+1] = 0
+                    bChanged = True
+            elif lCadCarateres[i] != 0 and lCadCarateres[i+1] == 0:
+                for j in range(i+2, len(sCadCarateres)):
+                    if lCadCarateres[j] != 0:
+                        if lCadCarateres[i].lower() == lCadCarateres[j].lower() and ((lCadCarateres[i].islower() and lCadCarateres[j].isupper()) or (lCadCarateres[j].islower() and lCadCarateres[i].isupper())):
+                            lCadCarateres[i] = 0
+                            lCadCarateres[j] = 0
+                            bChanged = True
+                            break
+    for i in range(len(sCadCarateres)):
+        if lCadCarateres[i] != 0:
+            sCorrected += lCadCarateres[i]
+    return sCorrected
+print(corrigir_palavra(input()))
